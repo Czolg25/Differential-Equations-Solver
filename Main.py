c2cc06903838f526831e16ff87cbc41f4a66c3de
Czolg25 <69245647+Czolg25@users.noreply.github.com>
2025-02-05 18:35:26 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -7,7 +7,11 @@ if __name__ == '__main__':
     task_repository = TasksRepository()
     task_service = TaskService(task_repository)
 
+    task_repository.add_task(FirstProblemSimpleTask())
+    task_repository.add_task(FirstProblemLossTask())
+    task_repository.add_task(SecondProblemSimpleTask())
     task_repository.add_task(SecondProblemLossTask())
+    task_repository.add_task(ThirdProblemSimpleTask())
 
-    task_service.solve(1000)
+    task_service.solve(10000)
 
